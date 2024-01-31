6d927049fa6701fe261d10d4d2153fbe67afea49
skullls <157409179+skullls@users.noreply.github.com>
2024-01-31 15:27:38 +0000

    ahora si si si

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -10,14 +10,41 @@ int main(int argc, char *argv[]) {
     int number = atoi(argv[1]); //Convert the console input argument to an integer
     
     /*It must be verified that the entered number is greater than or equal to zero*/
-    
+    if(number < 0)
+    {
+        perror("El núnmero debe ser mayor a cero");
+        exit(1);
+    }
     /*Implement the function that calculates the factorial of the input argument*/
+        int factorial = 1;
+    int b;
+    for (b = number; b > 1; b--)
+    {
+        factorial *= b;
+    }
     
     /*Print the result*/
-    
+    printf("El factorial de %d es %d",number,factorial);
     /*Implement the function that calculates if the argument is a prime number*/
-
+    int count = 0,index = 1;
+    while (index < number)
+    {
+        if(number%index == 0)
+            {
+            count++;
+            }
+        index++;
+    }
     /*Print the result*/
+        if(count == 1)
+    {
+        printf("%d es número primo\n",number);
+    }
+    else
+    {
+        printf("%d no es número primo\n",number);
+    }
     
     return 0;
+
 }
